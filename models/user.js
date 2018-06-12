943b8c3e776d586c9bbde2a9996a344901faa08e
alex <alex@localhost.localdomain>
2018-06-12 11:27:38 -0700

    a

---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullname: String, 
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+});
+
+userSchema.set('toObject', {
+    virtuals: true,     // include built-in virtual `id`
+    versionKey: false,  // remove `__v` version key
+    transform: (doc, ret) => {
+        delete ret._id; // delete `_id`
+        delete ret.password;
+    }
+});
+
+userSchema.methods.validatePassword = function (password) {
+    return password === this.password;
+};
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  
+  userSchema.statics.hashPassword = function (password) {
+    return bcrypt.hash(password, 10);
+  };
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/users.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+    const { fullname, username, password } = req.body;
+  
+    const newUser = { fullname, username, password };
+  
+    /***** Never trust users - validate input *****/
+    if (!username) {
+      const err = new Error('Missing `username` in request body');
+      err.status = 400;
+      return next(err);
+    }
+
+    if (!password) {
+        const err = new Error('Missing `password` in request body');
+        err.status = 400;
+        return next(err);
+      }
+  
+    User.create(newUser)
+      .then(result => {
+        res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+      })
+      .catch(err => {
+        if (err.code === 11000) {
+          err = new Error('Username already exists');
+          err.status = 400;
+        }
+        next(err);
+      });
+  });
+
+  module.exports = router;
